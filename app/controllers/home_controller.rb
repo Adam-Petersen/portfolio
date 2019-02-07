9e3ded0ff65ba618812acbe458e61df9047ad243
Adam-Petersen <adampetersenzzz@gmail.com>
2019-02-07 05:25:13 +0000

    adds home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def resume
+    send_file(
+      "#{Rails.root}/public/files/Resume.pdf",
+      filename: "adam-petersen-resume.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about/index'
   get 'projects/index'
   get 'projects/chess'
+  get 'home/resume'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
