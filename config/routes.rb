98d3a24da7c3dc84c4b69dff600ef22b5a978db2
Adam-Petersen <adampetersenzzz@gmail.com>
2019-02-18 03:48:50 +0000

    updates routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'contact/index'
-  get 'portfolio/index'
-  get 'about/index'
-  get 'projects/index'
-  get 'projects/chess'
-  get 'home/resume'
+  get '/contact', to: 'contact#index'
+  get '/portfolio', to: 'portfolio#index'
+  get '/projects', to: 'projects#index'
+  get '/projects/chess', to: 'projects#chess'
+  #get '/home/resume'
+  #get '/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
