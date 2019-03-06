6d9169558c266f1accf02cdcd72242fa76315c5e
Adam Petersen <pet00545@umn.edu>
2019-03-06 01:49:48 -0600

    updated linter and js to comply

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,50 +1,44 @@
-var modal = function() {
-  var init = function() {
-
-  };
-
-  $overlay = $('#overlay');
-  $modal = $('#modal');
-  $close = $('#close');
-  $open = $('#open');
-
-  $modal.hide();
-  $overlay.hide();
-  $open.click(function(e) {
-    e.preventDefault();
-    open();
-  });
-  $close.click(function(e) {
-    e.preventDefault();
-    close();
-  })
+var modal = () => {
+  const $overlay = $('#overlay');
+  const $modal = $('#modal');
+  const $close = $('#close');
+  const $open = $('#open');
 
   function center() {
-    var top, left;
-
-    top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
-    left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+    const top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+    const left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
 
     $modal.css({
       top: top + $(window).scrollTop(),
-      left: left + $(window).scrollTop()
+      left: left + $(window).scrollTop(),
     });
-  };
+  }
 
   function open() {
     center();
     $overlay.show();
     $modal.show();
-  };
+  }
 
   function close() {
     $overlay.hide();
     $modal.hide();
   }
 
+  $modal.hide();
+  $overlay.hide();
 
+  $open.click((e) => {
+    e.preventDefault();
+    open();
+  });
+
+  $close.click((e) => {
+    e.preventDefault();
+    close();
+  });
 };
 
-$(document).ready(function() {
+$(document).ready(() => {
   modal();
-})
+});
